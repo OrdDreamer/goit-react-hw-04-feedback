70873f6c63b36948245c781c4f9fde32f9210ab5
Roman Snitsarenko <59483945+OrdDreamer@users.noreply.github.com>
2023-03-10 20:47:50 +0200

    Use react hooks.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,51 +1,49 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import styles from './app.module.css';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
 
-export class App extends Component {
+export const App = () => {
 
-  state = {
+  const [state, setState] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  onLeaveFeedback = (value) => {
-    this.setState((prevState) => {
-      return {
+  const onLeaveFeedback = (value) => {
+    setState((prevState) => ({
+        ...prevState,
         [value]: prevState[value] + 1,
-      };
-    });
-  };
-
-  countTotalFeedback = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    return Math.floor((this.state.good / (this.countTotalFeedback() || 1)) * 100);
-  };
-
-  render() {
-    return (
-      <div className={styles.feedback}>
-        <Section title='Please leave feedback'>
-          <FeedbackOptions options={Object.keys(this.state)}
-                           onLeaveFeedback={this.onLeaveFeedback}
-          />
-        </Section>
-
-        <Section title='Statistics'>
-          <Statistics good={this.state.good}
-                      bad={this.state.bad}
-                      neutral={this.state.neutral}
-                      positivePercentage={this.countPositiveFeedbackPercentage()}
-                      total={this.countTotalFeedback()}
-          />
-        </Section>
-      </div>
+      }),
     );
-  }
-}
+  };
+
+  const countTotalFeedback = () => {
+    return state.good + state.neutral + state.bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    return Math.floor((state.good / (countTotalFeedback() || 1)) * 100);
+  };
+
+  return (
+    <div className={styles.feedback}>
+      <Section title='Please leave feedback'>
+        <FeedbackOptions options={Object.keys(state)}
+                         onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+
+      <Section title='Statistics'>
+        <Statistics good={state.good}
+                    bad={state.bad}
+                    neutral={state.neutral}
+                    positivePercentage={countPositiveFeedbackPercentage()}
+                    total={countTotalFeedback()}
+        />
+      </Section>
+    </div>
+  );
+};
